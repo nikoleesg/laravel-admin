8b6bd79dac4cbd1388fd40d70d3c72331ad9d660
Song <zosong@126.com>
2017-11-03 01:50:50 +0000

    Apply fixes from StyleCI

--- tests/config/filesystems.php
+++ tests/config/filesystems.php
@@ -72,9 +72,9 @@ return [
         'qiniu' => [
             'driver'  => 'qiniu',
             'domains' => [
-                'default'   => 'of8kfibjo.bkt.clouddn.com', //你的七牛域名
-                'https'     => 'dn-yourdomain.qbox.me',         //你的HTTPS域名
-                'custom'    => 'static.abc.com',                //你的自定义域名
+                'default' => 'of8kfibjo.bkt.clouddn.com', //你的七牛域名
+                'https'   => 'dn-yourdomain.qbox.me',         //你的HTTPS域名
+                'custom'  => 'static.abc.com',                //你的自定义域名
             ],
             'access_key' => 'your-access-key',  //AccessKey
             'secret_key' => 'your-secret-key',  //SecretKey
